1c23b5803587fa5b6f76a2c4c543910a3d8f280a
banoke254-svg <bano.ke.254@gmail.com>
2026-07-27 11:13:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+/* ===========================
+   BANO WEBSITE
+   Marble Mouse Effect
+=========================== */
 
+const marble = document.querySelector(".hero-marble");
+
+document.addEventListener("mousemove", function(event){
+
+    const x = (event.clientX / window.innerWidth - 0.5) * 40;
+
+    const y = (event.clientY / window.innerHeight - 0.5) * 40;
+
+    marble.style.transform =
+        `translate(${x}px, ${y}px)`;
+
+});
